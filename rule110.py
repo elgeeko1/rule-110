76c794a0dcf43cb335c094c88ca581041f321dcf
elgeeko-creator <jeff@creator.rest>
2023-10-06 00:21:27 -0600

    Working rule 110, no UI

--- rule110.py
+++ rule110.py
@@ -8,12 +8,15 @@ from time import sleep
 pygame.init()
 
 # number of cells per generation
-CELLS_PER_ROW = 80
-# number of rows to display
-DISPLAY_ROWS = 80
-
+CELLS_PER_ROW = 79
 # segment size of a square cell, in px
 CELL_SIZE_PX = 15
+# number of rows to display
+DISPLAY_ROWS = CELLS_PER_ROW
+# width of the canvas, in px
+DISPLAY_WIDTH = CELL_SIZE_PX * CELLS_PER_ROW
+# height of the canvas, in px
+DISPLAY_HEIGHT = CELL_SIZE_PX * DISPLAY_ROWS
 # time to highlight newly drawn cells, in ms
 CELL_DRAWTIME_MS = 0.0
 # delay between drawing rows, in ms
@@ -24,79 +27,97 @@ ROW_DRAWTIME_MS = 25.0
 # and results in faster response to user inputs
 EVENT_LOOP_MS = 10.0
 
+# automatically cycle generations?
 AUTOPLAY = True
 
-# return a new, blank canvas
-def surface_init() -> pygame.Surface:
-    # cavas dimensions in pixels (H, W)
-    dimensions = (
-        CELL_SIZE_PX * CELLS_PER_ROW,
-        CELL_SIZE_PX * DISPLAY_ROWS)
-    surface = pygame.display.set_mode(dimensions)
-    return surface
+###################
+# game logic
+###################
 
-# return the grid index for a generation
+# return a new, blank canvas sized to game grid
+#   returns: new canvas
+def canvas_init() -> pygame.Surface:
+    # cavas dimensions in pixels (H, W)
+    dimensions = ( DISPLAY_WIDTH, DISPLAY_HEIGHT )
+    canvas = pygame.display.set_mode(dimensions)
+    return canvas
+
+# grid row index for a generation
+#   returns: row index for the given generation
 def generation_to_row(generation: int) -> int:
     return max(min(generation, DISPLAY_ROWS - 1), 0)
 
-# return a new game grid containing an initial population
-def grid_init(cells0: list[bool]) -> list[list[bool]]:
-    if len(cells0) != CELLS_PER_ROW:
-        raise ValueError("ERROR: initial population size is " + len(cells0) + " but should be " + CELLS_PER_ROW)
+# initialize a new game grid containing an initial population
+#   seed_cells: seed population, list of booleans
+#   returns: 2D grid containing DISPLAY_ROWS, with seed population in first row
+def grid_init(seed_cells: list[bool]) -> list[list[bool]]:
+    if len(seed_cells) != CELLS_PER_ROW:
+        raise ValueError("ERROR: initial population size is "
+                         + len(seed_cells)
+                         + " but should be "
+                         + CELLS_PER_ROW)
     # 2D array, each row is a generation, row 0 is the intial popuation
     grid = [ [False] * CELLS_PER_ROW ] * DISPLAY_ROWS
-    grid[0] = cells0
+    grid[0] = seed_cells
     return grid
 
 # draw a grid onto the canvas
-def grid_draw(surface: pygame.Surface, grid: list[list[bool]]):
+#   canvas: canvas on which to draw
+#   grid: 2D grid to draw
+def grid_draw(canvas: pygame.Surface, grid: list[list[bool]]):
     # clear the canvas
-    surface.fill("black")
+    canvas.fill("black")
     # draw each row
     for (row_index, cells) in enumerate(grid):
-        grid_draw_row(surface, row_index, cells)
+        grid_draw_row(canvas, row_index, cells)
 
-# draw a single generation in the grid
-def grid_draw_row(
-        surface: pygame.Surface,
-        row_index: int,
-        population: list[bool],
-        cell_drawtime_ms: float = 0.0 ):
-    for (cell_index, cell) in enumerate(population):
+# draw a row of cells in the grid
+#   canvas: canvas on which to draw
+#   row_index: index of the row in the grid in which to paint the cells
+#   cells: cells to draw
+#   cell_drawtime_ms: time to highlight each cell before advancing to the next; defaults to zero
+def grid_draw_row(canvas: pygame.Surface,
+                  row_index: int,
+                  cells: list[bool],
+                  cell_drawtime_ms: float = 0.0 ):
+    # for each cell, highlight and paint according to its state
+    for (cell_index, cell) in  enumerate(cells):
         color = "black"
         if cell == True:
             color = "white"
-        # rectangle in surface coordinates representing this cell
-        # coordinate transformation: start from bottom of surface
-        cell_rect = pygame.Rect(
-            (   cell_index * CELL_SIZE_PX,
-                (DISPLAY_ROWS - row_index - 1) * CELL_SIZE_PX ) ,
-            (CELL_SIZE_PX, CELL_SIZE_PX) )
+        
+        # rectangle in canvas coordinates representing this cell
+        # coordinate transformation: start from bottom of canvas
+        cell_rect = pygame.Rect((cell_index * CELL_SIZE_PX,
+                                   (DISPLAY_ROWS - row_index - 1) * CELL_SIZE_PX ) ,
+                                (CELL_SIZE_PX, CELL_SIZE_PX) )
 
         # highlight cell being populated if animated
         if cell_drawtime_ms > 0.0:
-            pygame.draw.rect(surface, "green", cell_rect, 0)
+            pygame.draw.rect(canvas, "green", cell_rect, 0)
             pygame.display.update()
             sleep(cell_drawtime_ms / 1000.0)
 
         # draw filled rectangle
-        pygame.draw.rect(
-            surface,
-            color,
-            cell_rect
-        )
+        pygame.draw.rect(canvas,
+                         color,
+                         cell_rect)
 
         # draw cell outline (effectively creates gridlines)
-        pygame.draw.rect(surface, "teal", cell_rect, 1)
+        pygame.draw.rect(canvas, "teal", cell_rect, 1)
         if cell_drawtime_ms > 0.0:
             pygame.display.update()
 
-# generate a new population from a previous population
+# generate a new cell population from a previous populatio
+# according to Rule 110
 #   cells: row of cells
+#   returns: next generation of cells
 def rule110(cells: list[bool]) -> list[bool]:
+    # next generation of cells
     cells_next = [False] * CELLS_PER_ROW
+
+    # for each cell, apply Rule 110 and store in next generation
     for cell_index in range(len(cells)):
-        x =  (cell_index - 1) % CELLS_PER_ROW 
         left = cells[ (cell_index - 1) % CELLS_PER_ROW ]
         center = cells[ cell_index ]
         right = cells[ (cell_index + 1) % CELLS_PER_ROW ]
@@ -128,10 +149,19 @@ def rule110(cells: list[bool]) -> list[bool]:
 
     return cells_next
 
-def cycle(
-        surface: pygame.Surface,
-        grid: list[list[bool]],
-        generation: int) -> list[list[bool]]:
+# cycle the next generation of cells and draw to the canvas
+#
+# this method implements a circular queue in the grid. Once
+# the grid is full, the oldest generation is discarded and the
+# next generation is pushed to the top of the grid
+#
+#   canvas: canvas on which to draw
+#   grid: 2D array of cells
+#   generation: current generation count
+#   returns: 2D array of cells containing next generation
+def cycle(canvas: pygame.Surface,
+          grid: list[list[bool]],
+          generation: int) -> list[list[bool]]:
     # determine current and next generation indexes in the grid
     row_current_index = generation_to_row(generation)
     row_next_index = generation_to_row(generation + 1)
@@ -141,36 +171,39 @@ def cycle(
     if (generation + 1) >= DISPLAY_ROWS:
         grid = numpy.roll(grid, -1, axis=0)
         grid[row_next_index] = [False] * CELLS_PER_ROW
-        grid_draw(surface, grid)
+        grid_draw(canvas, grid)
 
     # calcuate next generation
     grid[row_next_index] = rule110(cells)
-    grid_draw_row(
-        surface,
-        row_next_index,
-        grid[row_next_index],
-        CELL_DRAWTIME_MS)
+    grid_draw_row(canvas,
+                  row_next_index,
+                  grid[row_next_index],
+                  CELL_DRAWTIME_MS)
 
     # udpate display
     pygame.display.update()
 
     return grid
 
+###################
+# main application
+###################
 
+# main application loop
 def main():
     # intial population with pattern [0, 0, 1, 0, 0, 1, ...]
-    cells0 = [False] * CELLS_PER_ROW
-    for cellindex in range(len(cells0)):
+    seed_cells = [False] * CELLS_PER_ROW
+    for cellindex in range(len(seed_cells)):
         if cellindex % 3 == 2:
-            cells0[cellindex] = True
-    grid = grid_init(cells0)
- 
+            seed_cells[cellindex] = True
+    grid = grid_init(seed_cells)
+
     # initialize display
     pygame.display.set_caption("Rule 110")
-    surface = surface_init()
-    grid_draw(surface, grid)
+    canvas = canvas_init()
+    grid_draw(canvas, grid)
     pygame.display.update()
-    
+
     # main program loop
     gameover = False
     generation = 0
@@ -184,12 +217,14 @@ def main():
             elif event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
                 gameover = True
 
+        # cycle the next generation
         if next_generation:
-            grid = cycle(surface, grid, generation)
+            grid = cycle(canvas, grid, generation)
             generation = generation + 1
             if ROW_DRAWTIME_MS > 0.0:
                 sleep(ROW_DRAWTIME_MS / 1000.0)
 
+        # delay to allow user input to be captured
         sleep(EVENT_LOOP_MS / 1000.0)
     
     print("game complete")
